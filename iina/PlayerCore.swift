f07e327d0e26150aeba2a2b190f01f617e60ebf7
Matt Svoboda <2213815+svobs@users.noreply.github.com>
2026-01-08 06:59:49 -0800

    Fix regression: ~3% CPU usage with no player open (UI timer incorrectly started)

--- iina/PlayerCore.swift
+++ iina/PlayerCore.swift
@@ -2845,6 +2845,8 @@ final class PlayerCore: NSObject {
     let useTimer: Bool
     if state.isAtLeast(.stopping) {
       useTimer = false
+    } else if info.currentPlayback == nil {
+      useTimer = false
     } else if info.isPaused {
       // Follow energy efficiency best practices and ensure IINA is absolutely idle when the
       // video is paused to avoid wasting energy with needless processing. If paused shutdown
